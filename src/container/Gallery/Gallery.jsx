ba35e6503bf6f192fd060f677e883d2eebd24c69
Jonatan Kwiatkowski <Joniqs91@gmail.com>
2023-07-07 09:43:05 +0100

    Added skeleton gallery to the project.

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import {
+  BsInstagram,
+  BsArrowLeftShort,
+  BsArrowRightShort,
+} from 'react-icons/bs';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Gallery.css';
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
-
+const Gallery = () => {
+  return (
+    <div className='app__gallery flex__center'>
+      <div className='app__gallery-content'>
+        <SubHeading title='Instagram' />
+        <h1 className='headtext__cormorant'>Photo Gallery</h1>
+        <p className='p__opensans' style={{ color: '#AAA', marginTop: '2rem' }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat
+          mattis ipsum turpis elit elit scelerisque egestas mu.
+        </p>
+      </div>
+    </div>
+  );
+};
 export default Gallery;
